a789090f4ffde215497c1c2bbccca117707bc7f9
정재웅 <jweidon0821@gmail.com>
2024-05-28 23:07:36 +0900

    Fix: 토큰 처리 로직 수정

--- users/urls.py
+++ users/urls.py
@@ -1,16 +1,10 @@
 from django.urls import path
 
-from .views import GoogleLoginView, GoogleCallbackView, LogoutView, RegisterView
+from .views import GoogleLoginView, GoogleCallbackView, LogoutView
 from rest_framework_simplejwt.views import TokenRefreshView
-
-from rest_framework import routers
-
-router = routers.DefaultRouter()
-# router.register('list', UserViewSet) # 유저리스트 (테스트용)
 
 urlpatterns = [
     path('google/login/', GoogleLoginView.as_view(), name='google_login'),
     path('google/callback/', GoogleCallbackView.as_view(), name='google_callback'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('register/', RegisterView.as_view(), name='register'),
 ]
