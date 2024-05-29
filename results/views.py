f514099a478a3aff14319d007d8ff51e7763ad6d
Juyoung1520 <202101524@hufs.ac.kr>
2024-05-30 00:36:48 +0900

    Bug : completion 버그 해결

--- results/views.py
+++ results/views.py
@@ -11,8 +11,22 @@ import json, os
 def major_requirement(major):
         major_name = major.lower()
 
-        if major_name == 'philosophy':
+        if major_name == '철학과':
             major_name = 'phil'
+        if major_name == '수학과':
+            major_name = 'math'
+        if major_name == '통계학과':
+            major_name = 'stat'
+        if major_name == '컴퓨터공학부' or major_name == '컴퓨터·전자시스템공학부':
+            major_name = 'cse'
+        if major_name == '반도체전자공학부(전자공학전공)':
+            major_name = 'ee'
+        if major_name ==  'Global Business&Technology학부':
+            major_name = 'gbt'
+        if major_name == '미디어커뮤니케이션학부':
+            major_name = 'media'
+                   
+        
 
         BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
         dir = os.path.join(BASE_DIR, 'requirement', 'fixtures')
@@ -29,12 +43,15 @@ def major_requirement(major):
 
 class GetResult(APIView):
     def get(self, request):
+        print("Get request received")
         try:
             basic = Basic.objects.get(user=request.user)
-            credit = Credit.objects.get_or_create(user=request.user)
+            #get_or_create는 튜플로 반환하는 함수임. 
+            credit, created = Credit.objects.get_or_create(user=request.user)
             extra = Extra.objects.get(user=request.user)
             major_subject = UserMajorCompulsory.objects.filter(user=request.user, status=False)
             liberal_subject = UserLiberalCompulsory.objects.filter(user=request.user, status=False)
+            print("Data fetched successfully from the database")
         except Basic.DoesNotExist:
             return Response({'detail': '입력된 기본 정보가 없습니다.'}, status=status.HTTP_404_NOT_FOUND)
         except Credit.DoesNotExist:
@@ -52,6 +69,9 @@ class GetResult(APIView):
         major_subject_serializer = UserMajorCompulsorySerializer(major_subject, many=True)
         liberal_subject_serializer = UserLiberalCompulsorySerializer(liberal_subject, many=True)
 
+        print(f"Serialized data: Basic: {basic_serializer.data}, Credit: {credit_serializer.data}, Extra: {extra_serializer.data}")
+
+
         student_no = basic_serializer.data['student_no']
         main_major = basic_serializer.data['main_major_name'] #학과명
 
@@ -59,7 +79,7 @@ class GetResult(APIView):
         result = {}
 
         for entry in requirement_data:
-            if entry['fields']['student_no'] == student_no[2:4]:
+            if entry['fields']['student_no'] == student_no:
                 if entry['fields']['major_type'] == 1:
                     # 졸업요건
                     re_main_major = entry['fields']['main_major']
@@ -120,7 +140,7 @@ class GetResult(APIView):
         # 시험 통과 여부
         u_main_test_pass = extra_serializer.data['main_test_pass']
         u_double_test_pass = extra_serializer.data['double_test_pass']
-        u_foreign_pass = extra_serializer.data['foreign_pass']
+        u_foreign_pass = extra_serializer.data['foreign_certification']
         
         if u_main_test_pass == False:
             result['main_test_pass'] = False
